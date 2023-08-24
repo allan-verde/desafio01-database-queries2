c20a0e87d176daea4cad16ba4a5fc0e25fcef715
Allan Verde <allangreen2906@gmail.com>
2023-08-24 10:22:41 -0300

    added integration test setup

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,12 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+export default async () => {
+  const defaultOptions = await getConnectionOptions();
+
+  return await createConnection(
+    Object.assign(
+      defaultOptions,
+      { host: process.env.NODE_ENV === "test" ? "localhost" : "database" }
+    )
+  )
+};
